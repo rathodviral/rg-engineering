535bddd62e6988c71c1df77db97bffa9966b9a84
vaibhav <patelvaibhav27@gmail.com>
2018-12-06 23:19:42 +0530

    fixed mainor issue

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AfterViewInit } from '@angular/core';
+declare var $: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      $(document).ready(function(){
+        $("#mobile-number").intlTelInput();
+      });
+    }, 2000);
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AfterViewInit } from '@angular/core/src/metadata/lifecycle_hooks';
+declare var $: any;
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
-
-  constructor() { }
+export class RegisterComponent implements OnInit, AfterViewInit {
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      $(document).ready(function(){
+        $("#mobile-number").intlTelInput();
+      });
+    }, 2000);
+  }
+
+  goto(type) {
+    if (type === 'login') {
+      this.router.navigate(['/login']);      
+    } else {
+      this.router.navigate(['/register']);      
+    }
   }
 
 }
