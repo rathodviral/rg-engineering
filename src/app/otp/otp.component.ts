0dd5c8f017326afc318f2966762fff76979bcb6e
vaibhav <patelvaibhav27@gmail.com>
2018-12-07 00:16:32 +0530

    added custom otp logic

--- src/app/otp/otp.component.ts
+++ src/app/otp/otp.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-otp',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OtpComponent implements OnInit {
 
-  constructor() { }
+  mobileNumber;
+  otp1;
+  otp2;
+  otp3;
+  otp4;
+  constructor(private toastrService: ToastrService) { }
 
   ngOnInit() {
+  }
+
+  submitMobile() {
+    if(this.mobileNumber) {
+      const otp = this.getOTP();
+      this.toastrService.success(otp.toString());
+      localStorage.setItem('otp', otp.toString());
+    }
+  }
+
+  getOTP() {
+    const val = Math.floor(1000 + Math.random() * 9000);
+    return val;    
+  }
+
+  confirmNumber() {
+    const getOTP = +localStorage.getItem('otp');
+    const otp = this.otp1 + this.otp2 + this.otp3 + this.otp4;
+    if(getOTP === +otp) {
+      this.toastrService.success('Mobile Number Verified Successfully');
+    } else {
+      this.toastrService.error('Try Again!');
+    }
   }
 
 }
--- src/app/otp/otp.module.ts
+++ src/app/otp/otp.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OtpComponent } from './otp.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot({timeOut: 2000}), // ToastrModule added
   ],
   declarations: [OtpComponent]
 })
